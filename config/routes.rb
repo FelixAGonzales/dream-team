4834f64151220caad3a6dd6f0541efea8f991ff9
Paulo Bandua <pcbandua@gmail.com>
2024-09-30 16:34:09 -0700

    create ACTION

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   get "/birds" => "birds#index"
 
   # Defines the root path route ("/")
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   get "/doggies" => "doggies#index"
   get "/candies" => "candies#index"
+  post "/candies" => "candies#create"
 end
