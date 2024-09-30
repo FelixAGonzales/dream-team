ebb70414b1b0b91c4c0a32415f51f603a5268295
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-30 18:46:34 -0500

    add update action for doggies

--- app/controllers/doggies_controller.rb
+++ app/controllers/doggies_controller.rb
@@ -1,6 +1,10 @@
 class DoggiesController < ApplicationController
   def index
-  render json: { message: "Who let the dogs out???"}
+    render json: { message: "Who let the dogs out???"}
+  end
+
+  def update
+    render json: { message: "Who let this dog in???"}
   end
 end
 
